904dc4f1bac3fb78c8faa750620aaab1b19bcc8d
Kevin Ruffus <kevin.ruffus@rentanerdconsulting.com>
2017-02-26 18:14:13 -0600

    Add files via upload

--- Bash/showcron.sh
+++ Bash/showcron.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+
 # System-wide crontab file and cron job directory. Change these for your system.
 CRONTAB='/etc/crontab'
 CRONDIR='/etc/cron.d'
 
+
 # Single tab character. Annoyingly necessary.
 tab=$(echo -en "\t")
+
 
 # Given a stream of crontab lines, exclude non-cron job lines, replace
 # whitespace characters with a single space, and remove any spaces from the
@@ -16,6 +19,7 @@ clean_cron_lines()
 		echo "${line}" | egrep --invert-match '^($|\s*#|\s*[[:alnum:]_]+=)' | sed --regexp-extended "s/\s+/ /g" | sed --regexp-extended "s/^ //"
 	done;
 }
+
 
 # Given a stream of cleaned crontab lines, echo any that don't include the
 # run-parts command, and for those that do, show each job file in the run-parts
@@ -29,7 +33,7 @@ lookup_run_parts()
 			echo "${line}"
 		else
 			cron_fields=$(echo "${line}" | cut -f1-6 -d' ')
-			cron_job_dir=$(echo  "${match}" | awk '{print $NF}')
+			cron_job_dir=$(echo "${match}" | awk '{print $NF}')
 
 			if [[ -d "${cron_job_dir}" ]] ; then
 				for cron_job_file in "${cron_job_dir}"/* ; do  # */ <not a comment>
@@ -40,20 +44,25 @@ lookup_run_parts()
 	done;
 }
 
+
 # Temporary file for crontab lines.
 temp=$(mktemp) || exit 1
+
 
 # Add all of the jobs from the system-wide crontab file.
 cat "${CRONTAB}" | clean_cron_lines | lookup_run_parts >"${temp}"
 
+
 # Add all of the jobs from the system-wide cron directory.
 cat "${CRONDIR}"/* | clean_cron_lines >>"${temp}"  # */ <not a comment>
+
 
 # Add each user's crontab (if it exists). Insert the user's name between the
 # five time fields and the command.
 while read user ; do
 	crontab -l -u "${user}" 2>/dev/null | clean_cron_lines | sed --regexp-extended "s/^((\S+ +){5})(.+)$/\1${user} \3/" >>"${temp}"
 done < <(cut --fields=1 --delimiter=: /etc/passwd)
+
 
 # Output the collected crontab lines. Replace the single spaces between the
 # fields with tab characters, sort the lines by hour and minute, insert the
